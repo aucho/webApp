97a630f9e6e16cd2e549036477c5137b5c4d86eb
aucho <mdxsyl@163.com>
2019-09-18 13:13:15 +0800

    ...

---
+++ src/views/shopPage/shopPage.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      shopInfo:{
+        name:'',
+        score:'',
+        
+      }
+    }
+  },
+  Create() {
+    // 发送ajax get请求 获取商铺的 自身信息和商品信息
+
+  },
+}
+</script>
+
+<style lang="stylus">
+
+</style>
